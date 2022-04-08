f13e086a59b28d02fc304a7a93863e464d358437
HP USER <HP USER@LAPTOP-6UPDUBH9>
2022-04-07 23:30:44 -0500

    Completo Tarea

--- src/main/java/com/mycompany/g07_poo_ejercicio_01_02_cgmm_cvlg/Empleado.java
+++ src/main/java/com/mycompany/g07_poo_ejercicio_01_02_cgmm_cvlg/Empleado.java
@@ -12,32 +12,65 @@ public class Empleado {
     String nombre;
     int horasTrabajadas;
     double costoHora;
-    int anioingreso;
+    int anioIngreso;
     public double calcularAPagar(){
         var retorno=1000d;
         if(this.horasTrabajadas>=0&&this.horasTrabajadas<=300){
             retorno=this.horasTrabajadas*this.costoHora;
         }
         return retorno;
-    } 
-public int calcularYear(int currentYear){
-      var retorno=10000;
-      if(this.anioingreso>=0&&this.anioingreso<=currentYear){
-          retorno=currentYear-anioingreso;
-      }else{
-          retorno=0;
-      }
-      return retorno;
-  
-}
-public double calcularIngresos(int calcularYear){
+    }
+    /**public int calcularYear(int currentYear){
+        var retorno=10000;
+        if(this.anioingreso>=0&&this.anioingreso<=currentYear){
+             retorno=currentYear-anioingreso;
+         }else{
+             retorno=0;
+         }
+         return retorno;
+}       
+    public double calcularIngresos(int calcularYear){
         var retorno=2022;
         if(this.calcularYear(calcularYear)>=0 && this.calcularYear(calcularYear)<=2022){
             retorno=(int) (this.calcularYear(calcularYear)*0.2);
         return retorno;
     }
-        return retorno;
-        
-        
+        return retorno; 
 }
+**/
+    public double calcularIngreso(int anioActual){
+        
+        var sueldo = 1000d;
+        var edadEmpleado = 1000d;
+        var multiplicacion1 = 1000d;
+        var multiplicacion2 = 1000d;
+        var suma = 1000d;
+            sueldo = horasTrabajadas * costoHora;
+            edadEmpleado = anioActual - anioIngreso;
+            multiplicacion1 = sueldo * 0.02; 
+            multiplicacion2 = edadEmpleado * multiplicacion1;
+            suma = sueldo + multiplicacion2;    
+        return suma;
+    } 
+    public double calcularHoraBonusExtra(int maximoHorasTrabajadas){  
+        var retorno = 1000d;
+        var multiplicacion3 = 1000d; 
+        if(this.horasTrabajadas >= 0 && this.horasTrabajadas <= 150);
+            retorno =(this.horasTrabajadas - maximoHorasTrabajadas);
+            multiplicacion3 = (this.costoHora * retorno);
+        return multiplicacion3;
+    } 
+    public double calcularImpuestos(int limit1, int limit2, int limit3 ){    
+        var retorno=100d;
+        if(this.costoHora*horasTrabajadas >= 0 && this.costoHora*horasTrabajadas <= limit1){
+            if(this.costoHora*horasTrabajadas >= limit1 && this.costoHora*horasTrabajadas<= limit2)
+                retorno=this.costoHora*horasTrabajadas*0.05;
+            else
+                retorno=this.costoHora*horasTrabajadas*0.12;
+        }else{
+             if(this.costoHora*horasTrabajadas>=limit2 && this.costoHora*horasTrabajadas<=limit3)
+                retorno=this.costoHora*horasTrabajadas*0.25;
+        }
+        return retorno;
+   }
 }
--- src/main/java/com/mycompany/g07_poo_ejercicio_01_02_cgmm_cvlg/Pirncipal.java
+++ src/main/java/com/mycompany/g07_poo_ejercicio_01_02_cgmm_cvlg/Pirncipal.java
@@ -16,16 +16,18 @@ public class Pirncipal {
     public static void main(String[] args) {
         var empleado1=new Empleado();
         empleado1.nombre="Luis";
-        empleado1.anioingreso= 2010;
+        empleado1.anioIngreso= 2010;
         empleado1.horasTrabajadas=160;
         empleado1.costoHora=11.50;
         System.out.println(empleado1.nombre+" | "+
-                           empleado1.anioingreso+" | "+
+                           empleado1.anioIngreso+" | "+
                            empleado1.horasTrabajadas+" | "+
                            empleado1.costoHora+" | ");
         System.out.println("El valor total a pagar es: "+empleado1.calcularAPagar());
-        System.out.println("El valor de los ingresos totales son: "+empleado1.calcularIngresos(0));
-        System.out.println("Los años trabajados totales son: "+empleado1.calcularYear(2022));
+
+        System.out.println("Los años trabajados totales son: "+empleado1.calcularIngreso(2022));
+  
+        System.out.println("Los impuestos son: "+empleado1.calcularImpuestos(500, 1000, 2000));
     }
 }
 
